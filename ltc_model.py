50449fe2e454d98d61effb4b3edb4db9c9a564c0
Mehmood <mehmoodali4@gmail.com>
2019-05-07 15:07:16 +0200

    Implemented feed forward for the model

--- ltc_model.py
+++ ltc_model.py
@@ -7,7 +7,7 @@ Created on Tue May  7 12:57:06 2019
 
 import torch
 import torch.nn as nn
-from mypath import Path
+
 import math
 
 class LTC(nn.Module):
@@ -15,10 +15,10 @@ class LTC(nn.Module):
     The LTC network.
     """
 
-    def __init__(self, num_classes, pretrained=False,opt):
-        super(C3D, self).__init__()
+    def __init__(self, num_classes,opt, d, pretrained=False):
+        super(LTC, self).__init__()
 
-        self.conv1 = nn.Conv3d(opt.sampleSize[1], 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
+        self.conv1 = nn.Conv3d(opt[1], 64, kernel_size=(3, 3, 3), padding=(1, 1, 1))
         self.t1 = nn.Threshold(0, 0,True)
         self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
 
@@ -38,21 +38,64 @@ class LTC(nn.Module):
         self.t5 = nn.Threshold(0, 0,True)
         self.pool5 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
         
-        oT = math.floor(opt.sampleSize[2]/16); # 4 times max pooling of 1/2
-        oH = math.floor(opt.sampleSize[3]/32); # 5 times max pooling of 1/2
-        oW = math.floor(opt.sampleSize[4]/32); # 5 times max pooling of 1/2
+        oT = math.floor(opt[2]/16); # 4 times max pooling of 1/2
+        oH = math.floor(opt[3]/32); # 5 times max pooling of 1/2
+        oW = math.floor(opt[4]/32); # 5 times max pooling of 1/2
         
         self.fc6 = nn.Linear(256*oT*oH*oW, 2048)
         self.t6 = nn.Threshold(0, 0,True)
-        self.dropout = nn.Dropout(opt.dropout)
+        self.dropout1 = nn.Dropout(d)
         self.fc7 = nn.Linear(2048, 2048)
         self.t7 = nn.Threshold(0, 0,True)
-        self.dropout = nn.Dropout(opt.dropout)
+        self.dropout2 = nn.Dropout(d)
         self.fc8 = nn.Linear(2048, num_classes)
         self.softmax = nn.LogSoftmax()
+        self.relu = nn.ReLU()
 
 
     def forward(self, x):
+        opt = inputs.size()
+        x = self.relu(self.conv1(x))
+        x = self.t1(x)
+        x = self.pool1(x)
+
+        x = self.relu(self.conv2(x))
+        x = self.t2(x)
+        x = self.pool2(x)
+
+        x = self.relu(self.conv3(x))
+        x = self.t3(x)
+        x = self.pool3(x)
+
+        x = self.relu(self.conv4(x))
+        x = self.t4(x)
+        x = self.pool4(x)
+
+        x = self.relu(self.conv5(x))
+        x = self.t5(x)
+        x = self.pool5(x)
+        
+        oT = math.floor(opt[2]/16); # 4 times max pooling of 1/2
+        oH = math.floor(opt[3]/32); # 5 times max pooling of 1/2
+        oW = math.floor(opt[4]/32); # 5 times max pooling of 1/2
+
+        x = x.view(256*oT*oH*oW)
+        x = self.relu(self.fc6(x))
+        x = self.t6(x)
+        x = self.dropout1(x)
+        x = self.relu(self.fc7(x))
+        x = self.t7(x)
+        x = self.dropout2(x)
+
+        result = self.softmax(self.fc8(x))
+
+        return result
+    
+    
+inputs = torch.rand(1, 3, 16, 112, 112)
+net = LTC(num_classes=101,opt = inputs.size(),d =0.5, pretrained=True)
+outputs = net.forward(inputs)
+print(outputs.size())
 
         
 
